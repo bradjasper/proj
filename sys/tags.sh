ee62ba99b8b08dfa2985b3f2ee61d2100db08f31
Brad Jasper <bjasper@gmail.com>
2009-10-29 19:39:36 -0500

    added update interval to tags script

--- sys/tags.sh
+++ sys/tags.sh
@@ -1,4 +1,11 @@
 # Generate tags for this project
 
-find -X $PROJECT_DIR -name \*.py -print 2> /dev/null \
-    | xargs ptags > $PROJECT_DIR/.tags
+TAGS_FILE=$PROJECT_DIR/.tags
+
+# Amount of time (in minutes) to wait before update the tags file again
+UPDATE_INTERVAL=5
+
+# Check if the file hasn't been updated for at least $UPDATE_INTERVAL
+if test `find "$TAGS_FILE" -mmin +$UPDATE_INTERVAL`; then
+    find -X $PROJECT_DIR -name \*.py -print 2> /dev/null | xargs ptags > $TAGS_FILE
+fi
